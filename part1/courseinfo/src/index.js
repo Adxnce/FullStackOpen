3d556dc23b78a34a93e1538207336117f10aef05
Adán Sánchez <adsanchez0901@gmail.com>
2023-11-29 22:46:10 -0300

    ejercicios subidos

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,17 +1,85 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  // console.log(course.parts[0].exercises)
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+const Header = ({course}) => {
+  return (
+    <h1>
+      {course.name}
+    </h1>
+  )
+}
+
+const Content = ({parts}) => {
+  return(
+    <div>
+      <Part name={parts[0].name} exercises={parts[0].exercises}></Part>
+      <Part name={parts[1].name} exercises={parts[1].exercises}></Part>
+      <Part name={parts[2].name} exercises={parts[2].exercises}></Part>
+    </div>
+  )
+}
+
+const Part = ({name, exercises}) => {
+  return(
+    <p>
+      {name} {exercises}
+    </p>
+  )
+}
+
+const Total = ({parts}) => {
+  return(
+    <div>
+      {parts[0].exercises  + parts[1].exercises + parts[2].exercises}
+    </div>
+  )
+}
+
+//   return (
+//     <div>
+//       <h1>{course}</h1>
+//       <p>
+//         {part1} {exercises1}
+//       </p>
+//       <p>
+//         {part2} {exercises2}
+//       </p>
+//       <p>
+//         {part3} {exercises3}
+//       </p>
+//       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+//     </div>
+//   )
+// }
+
+ReactDOM.render(<App />, document.getElementById('root'))
--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,17 +1,69 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const increaseGood = () => setGood(good + 1)
+  const increaseBad = () => setBad(bad + 1)
+  const increaseNeutral = () => setNeutral(neutral + 1)
+
+  return (
+    <div>
+      <h2>Give Feedback</h2>
+        <Button handleClick={increaseGood} text={"good"}/>
+        <Button handleClick={increaseNeutral} text={"neutral"}/>
+        <Button handleClick={increaseBad} text={"bad"}/>
+      
+      <h2>Statistics</h2>
+        <Statistics good={good} bad={bad} neutral={neutral} ></Statistics>
+    </div>
+  )
+}
+
+
+const Button = ({handleClick, text}) => {
+  return(
+    <button onClick={handleClick}>{text}</button>
+  )
+}
+
+const StatisticLine = ({text, value}) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+const Statistics = ({good, bad, neutral}) => {
+
+  const all = (good + bad + neutral)
+  const average = (good - bad)/all
+  const positive = good/all
+
+  if (all == 0) {
+    return "No feedback Given"
+  }
+
+  return(
+    <table>
+      <StatisticLine text="good" value={good}></StatisticLine>
+      <StatisticLine text="neutral"value={neutral}></StatisticLine>
+      <StatisticLine text="bad" value={bad}></StatisticLine>
+      <StatisticLine text="all"value={all}></StatisticLine>
+      <StatisticLine text="average"value={average}></StatisticLine>
+      <StatisticLine text="positive"value={positive}></StatisticLine>
+    </table>
+  )
+}
+
+
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
